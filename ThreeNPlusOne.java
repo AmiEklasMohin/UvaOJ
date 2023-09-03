2be709e062922b94cf1663e8374c618437448232
AmiEklasMohin <mohin.gacc@gmail.com>
2023-09-03 22:08:50 +0600

    Uva Online Judge 100 - The 3n + 1 problem

---
+++ ThreeNPlusOne.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class ThreeNPlusOne {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            int i = sc.nextInt();
+            int j = sc.nextInt();
+            int a = i, b = j;
+            int maxCycleLength = 0;
+            if (i > j) {
+                int temp = i;
+                i = j;
+                j = temp;
+            }
+            for (int x = i; x <= j; x++) {
+                int temp = x, count = 1;
+                while (temp != 1) {
+                    if (temp % 2 == 0) {
+                        temp = temp  / 2;
+                    } else {
+                        temp = (temp * 3) + 1;
+                    }
+                    count++;
+                }
+                maxCycleLength = Math.max(maxCycleLength, count);
+            }
+            System.out.println(a + " " + b + " " +maxCycleLength);
+        }
+        sc.close();
+    }
+}
