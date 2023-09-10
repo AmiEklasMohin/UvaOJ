4f678be0f9f44179dc65abfa75a5b4618d73067d
AmiEklasMohin <mohin.gacc@gmail.com>
2023-09-10 18:15:42 +0600

    Uva 10055 - Hashmat the Brave Warrior

--- HashmatTheBraveWarrior.cpp
+++ HashmatTheBraveWarrior.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <cmath>
-using namespace std;
 
 int main() {
     long sizeOfHashmatsArmy, opponentsArmySize, absoluteDifference;
-    while (cin >> sizeOfHashmatsArmy >> opponentsArmySize) {
-        absoluteDifference = abs(sizeOfHashmatsArmy - opponentsArmySize);
-        cout << absoluteDifference << endl;
+    while (std::cin >> sizeOfHashmatsArmy >> opponentsArmySize) {
+        absoluteDifference = std::abs(sizeOfHashmatsArmy - opponentsArmySize);
+        std::cout << absoluteDifference << std::endl;
     }
     return 0;
 }
